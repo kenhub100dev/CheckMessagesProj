0e1adb928145c13530322755c81c157fd4604f8f
kenhub100dev <kentayl_99@yahoo.com>
2016-06-03 23:35:32 -0400

    changes to 11:35pm

--- src/com/db/MqttClientEnt.java
+++ src/com/db/MqttClientEnt.java
@@ -6,35 +6,23 @@
 package com.db;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Objects;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  *
  * @author kenta_000
  */
 @Entity
-public class MessageEnt implements Serializable {
+public class MqttClientEnt implements Serializable {
 
     private static long serialVersionUID = 1L;
-
-    public MessageEnt() {
-        this.id = new Long(0);
-        this.name = "na";
-        this.topic = "na";
-    }
-
-    public MessageEnt(Long id, String name, String topic) {
-        this.id = id;
-        this.name = name;
-        this.topic = topic;
-    }
-    
-    
-    
 
     /**
      * @return the serialVersionUID
@@ -53,11 +41,27 @@ public class MessageEnt implements Serializable {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     
-    
     private String name;
     
+    private Integer age;
     
-    private String topic;
+    private String note;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "id")
+    private ArrayList<MessageEnt> ments;
+
+    public MqttClientEnt() {
+    }
+
+    public MqttClientEnt(Long id, String name, Integer age, String note, ArrayList<MessageEnt> ments) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.note = note;
+        this.ments = ments;
+    }
+    
+    
     
 
     public Long getId() {
@@ -83,30 +87,60 @@ public class MessageEnt implements Serializable {
     }
 
     /**
-     * @return the topic
+     * @return the age
      */
-    public String getTopic() {
-        return topic;
+    public Integer getAge() {
+        return age;
     }
 
     /**
-     * @param topic the topic to set
+     * @param age the age to set
      */
-    public void setTopic(String topic) {
-        this.topic = topic;
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the note
+     */
+    public String getNote() {
+        return note;
+    }
+
+    /**
+     * @param note the note to set
+     */
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    /**
+     * @return the ments
+     */
+    public ArrayList<MessageEnt> getMents() {
+        return ments;
+    }
+
+    /**
+     * @param ments the ments to set
+     */
+    public void setMents(ArrayList<MessageEnt> ments) {
+        this.ments = ments;
     }
 
     @Override
     public String toString() {
-        return "MessageEnt{" + "id=" + id + ", name=" + name + ", topic=" + topic + '}';
+        return "MqttClientEnt{" + "id=" + id + ", name=" + name + ", age=" + age + ", note=" + note + ", ments=" + ments + '}';
     }
 
     @Override
     public int hashCode() {
         int hash = 7;
-        hash = 43 * hash + Objects.hashCode(this.id);
-        hash = 43 * hash + Objects.hashCode(this.name);
-        hash = 43 * hash + Objects.hashCode(this.topic);
+        hash = 53 * hash + Objects.hashCode(this.id);
+        hash = 53 * hash + Objects.hashCode(this.name);
+        hash = 53 * hash + Objects.hashCode(this.age);
+        hash = 53 * hash + Objects.hashCode(this.note);
+        hash = 53 * hash + Objects.hashCode(this.ments);
         return hash;
     }
 
@@ -121,23 +155,28 @@ public class MessageEnt implements Serializable {
         if (getClass() != obj.getClass()) {
             return false;
         }
-        final MessageEnt other = (MessageEnt) obj;
+        final MqttClientEnt other = (MqttClientEnt) obj;
         if (!Objects.equals(this.name, other.name)) {
             return false;
         }
-        if (!Objects.equals(this.topic, other.topic)) {
+        if (!Objects.equals(this.note, other.note)) {
             return false;
         }
         if (!Objects.equals(this.id, other.id)) {
             return false;
         }
+        if (!Objects.equals(this.age, other.age)) {
+            return false;
+        }
+        if (!Objects.equals(this.ments, other.ments)) {
+            return false;
+        }
         return true;
     }
-
- 
-
     
     
+    
+
 
     
 }
